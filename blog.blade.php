e76a777248a8265636724549c7e00f75b00f0308
rivaldyhisham <56820072+rivaldyhisham@users.noreply.github.com>
2019-11-03 10:46:51 +0700

    Add files via upload

---
+++ blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fattah laundry</title>
+</head>
+<body>
+ 
+	<h3>Progress laundry</h3>
+	<p>Ini merupakan progress yang dibuat untuk tugas PBO</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+ 
+</body>
+</html>
